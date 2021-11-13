6501391aa09bd12444fb6099d736ec2c8d8fcf09
Andriy <dioxine2000@gmail.com>
2021-11-13 16:54:51 +0200

    Add lazy load to image

--- src/scenes/components/Product/ProductPicture.js
+++ src/scenes/components/Product/ProductPicture.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
 export function ProductPicture({ item, className }) {
-	return <img src={item.picture} alt={'description'} className={className} />;
+	return (
+		<LazyLoadImage
+			src={item.picture}
+			alt={'description'}
+			className={className}
+			effect={'blur'}
+		/>
+	);
 }
